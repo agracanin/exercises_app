9e75bcb0ac5fc871a33601821d409d8a0a7f500f
Alen <alengracanin@gmail.com>
2024-09-14 11:21:12 -0400

    started exercise search display

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
-import { exercise, exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
 
@@ -21,19 +21,19 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 
     const handleSearch = async () => {
         if (search) {
-            const exercisesData = await fetchData
-                ('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+            const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
 
             const searchedExercises = exercisesData.filter(
-                (exercise) => exercise.name.toLowerCase().includes(search)
-                    || exercise.target.toLowerCase().includes(search)
-                    || exercise.equipment.toLowerCase().includes(search)
-                    || exercise.bodyPart.toLowerCase().includes(search)
+                (item) => item.name.toLowerCase().includes(search)
+                    || item.target.toLowerCase().includes(search)
+                    || item.equipment.toLowerCase().includes(search)
+                    || item.bodyPart.toLowerCase().includes(search),
             );
 
 
             setSearch('');
-            setExercises(searchedExercises);
+            setExercises(searchedExercises)
+            console.log(searchedExercises);
         }
     }
 
